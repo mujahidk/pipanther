32b9895fee03054d79aaddfda0b8f599c37b611b
Mujahid Khaleel <mujahidkhaleel@gmail.com>
2016-04-10 19:44:02 -0400

    Redirect Flask log to 'log' sub-directory on run.

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-python src/run.py >> access.log 2>&1 &
+# Create log directory
+mkdir -p log
+
+# Start the Flask server in background, redirect output to log directory.
+python src/run.py >> log/access.log 2>&1 &
